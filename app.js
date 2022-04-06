e38b9b09e2e78d0dc8544aaee19f9f45512dc1f9
unknown <camilomacoris@hotmail.com>
2022-04-06 14:42:27 -0300

    refatorando o código em funções

--- app.js
+++ app.js
@@ -1,40 +1,54 @@
-const correctAnswers = ['A','D','B','C']
 const form = document.querySelector('.questions')
-const finalResult = document.querySelector('.finalResult')
+const finalScore = document.querySelector('.final-score')
+
+const correctAnswers = ['A','D','B','C']
+
+let score = 0
+
+const getUserAnswers = () => {
+    const userAnswers = []
+
+    correctAnswers.forEach((_,index) => {
+        userAnswers.push(form[`inputAnswer${index + 1}`].value)
+    }) 
+    
+    return userAnswers
+}
+
+const calcultateUserScore = userAnswers => {
+    score = 0
+    userAnswers.forEach((userAnswer,index) => {
+        if (userAnswer === correctAnswers[index]) {
+            score += 25
+        }
+    })
+}
+
+const showFinalScore = () => {
+    finalScore.style.display = 'flex'
+    scrollTo({
+        top:0,
+        left:0,
+        behavior:'smooth'
+    })
+}
+
+const animationFinalScore = () => {
+    let cont = 0
+    const animation = setInterval(() => {        
+        if (cont === score) {
+            clearInterval(animation)
+        } 
+            finalScore.textContent = `Sua pontuação foi de: ${cont}%`
+            cont++
+    },10)  
+}
 
 form.addEventListener('submit', event => {
     event.preventDefault()
 
-    const userAnswers = [
-        form.inputAnswer1.value,
-        form.inputAnswer2.value,
-        form.inputAnswer3.value,
-        form.inputAnswer4.value,
-    ]
-
-    let score = 0
-    let cont = 0
-
-    const calculateScore = (answer,index) =>{
-        if(answer === correctAnswers[index]){
-            score += 25
-        }
-    }
-    const printScoreOnScreen = () => {
-        if(cont === score){
-            clearInterval(pontuationAnimation)
-        } 
-        finalResult.textContent = `Sua pontuação foi de: ${cont}%`
-        cont++
-    }
-    
-    userAnswers.forEach(calculateScore)
-
-    finalResult.style.display = 'flex'
-
-    scrollTo(0,0)
-
-    const pontuationAnimation = setInterval(printScoreOnScreen,10)
-    
-    
+    const userAnswers = getUserAnswers()
+    calcultateUserScore(userAnswers)
+    showFinalScore()
+    animationFinalScore()
 })
